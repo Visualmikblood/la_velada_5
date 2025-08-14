671c73118b46fdd6afcb609ba69272cbc71abedf
Miguel <1bcmigueliglesias@gmail.com>
2025-08-14 12:08:22 +0200

    he puesto la img de entradas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Header from './components/header/header.jsx';
 import Pronostico from './components/pronostico/pronostico.jsx';
+import Entradas_a_la_venta from './components/entradas_a_la_venta/entradas_a_la_venta.jsx';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="app-container">
       <Header />
       <Pronostico />
+      <Entradas_a_la_venta />
     </div>
   );
 }
---
+++ src/components/entradas_a_la_venta/entradas_a_la_venta.jsx
@@ -0,0 +1,28 @@
+
+import React, { useEffect, useRef } from 'react';
+import './entradas_a_la_venta.scss';
+
+
+const Entradas_a_la_venta = () => {
+
+    return (
+
+
+        <section className='section_de_entradas_a_la_venta'>
+            <div className='div_de_img_de_la_seccion_de_entradas'>
+                <img src="/images/seccion_venta_de_entradas/entradas-la-velada.webp" alt="" />
+            </div>
+            <a className ="a_de_entradas_a_la_venta" href="">
+                ¡entradas ya a la venta!
+            </a>
+        </section>
+
+    );
+
+
+
+
+
+    };
+
+    export default Entradas_a_la_venta;
